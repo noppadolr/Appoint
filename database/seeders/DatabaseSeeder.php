35fcda880600217eef394e6c885f18f8e1d544ef
noppadolr <riyasarn.n@gmail.com>
2024-02-18 13:34:11 +0700

    Role DB:seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Role;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        Role::query()->create([
+            'name' => 'doctor',
+            'created_at' => Carbon::now(),
+            'updated_at'=> Carbon::now()
+        ]);
+        Role::query()->create(['name' => 'admin',
+            'created_at' => Carbon::now(),
+            'updated_at'=> Carbon::now()]);
+        Role::query()->create(['name' => 'patient',
+            'created_at' => Carbon::now(),
+            'updated_at'=> Carbon::now()]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
